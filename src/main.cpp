aee96ae1c01252fb96ede50bcfaa38cf3a543a4a
A. Claden <a.claden@purplespark.org>
2021-04-02 00:16:38 -0400

    test 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,3 @@
-/**********
- * Arduino 3d printer enclosure environmental control
- * I needed to build an enclosure for my 3D printers, and as it was going to be in a pretty cold room,
- * I needed to create some environmental control to heat it up, but also to cool it down.
- * The purpose is to keep the temperature inside the enclosure at a constant 21C.
- * In order to do so, several fans will be used:
- * 1. Heating fan
- * 2. Cooling fan
- * 3. Exhaust fan
- * 
- * All those fans will be controlled by an Arduino.
- * Some temperature sensors will also be used to start and stop the fans when the required temperature
- * is correct inside the enclosure. 
- **********/
-
 #include <Arduino.h>
 
 // Min temperature inside the enclosure
