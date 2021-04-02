63b54b8b8af7281a39455276ea4dcd5e46220809
A. Claden <a.claden@purplespark.org>
2021-04-02 10:19:45 -0400

    Add temp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
 #include "DHT.h"
 #include <LiquidCrystal.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // Min temperature inside the enclosure
 #define MIN_TEMP 19.00
 // Max temperature inside the enclosure
 #define MAX_TEMP 24.00
+
+// Define the fan relay pins
+#define RELAY_PIN_COLD_FAN 10
+#define RELAY_PIN_HEAT_FAN 11
+#define RELAY_PIN_EXHAUST_FAN 12
+
+// Define the temperature probe pin
+#define ONEWIRE_BUS_PIN 5
 
 // DHT sensor configuration
 #define DHTPIN 2
@@ -16,6 +26,12 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // Create LiquidCrystal object
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+// Create the sensor object
+OneWire oneWire(ONEWIRE_BUS_PIN);
+DallasTemperature sensors(&oneWire);
+float celsius = 0.00;
+float farenheit = 0.00;
 
 // Declare functions
 void displayTempHumi(float, float);
