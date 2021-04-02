4ae5e66f3020c06f1cbe6d762be8db6c702ebd9a
A. Claden <a.claden@purplespark.org>
2021-04-02 00:10:32 -0400

    Set temps

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,11 @@
 
 #include <Arduino.h>
 
+// Min temperature inside the enclosure
+#define MIN_TEMP 19.00
+// Max temperature inside the enclosure
+#define MAX_TEMP 24.00
+
 void setup() {
   // put your setup code here, to run once:
 }
